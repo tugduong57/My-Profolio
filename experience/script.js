1600070b6acff054b337132ddd298d089937c8a9
Tung Duong <tugduongnguyen@gmail.com>
2025-12-06 12:44:56 +0700

    Bỏ hiệu ứng

--- experience/script.js
+++ experience/script.js
@@ -17,17 +17,17 @@ $(document).ready(function(){
     });
 });
 
-/* ===== SCROLL REVEAL ANIMATION ===== */
-const srtop = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 1000,
-    reset: true
-});
+// /* ===== SCROLL REVEAL ANIMATION ===== */
+// const srtop = ScrollReveal({
+//     origin: 'top',
+//     distance: '80px',
+//     duration: 1000,
+//     reset: true
+// });
 
-/* SCROLL EXPERIENCE */
-srtop.reveal('.experience .timeline',{delay: 400});
-srtop.reveal('.experience .timeline .container',{interval: 400}); 
+// /* SCROLL EXPERIENCE */
+// srtop.reveal('.experience .timeline',{delay: 400});
+// srtop.reveal('.experience .timeline .container',{interval: 400}); 
 
 
 // Start of Tawk.to Live Chat
@@ -62,7 +62,7 @@ document.onkeydown = function(e) {
   }
 }
 
-document.addEventListener('visibilitychange',
+/* document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
         document.title = "Experience | Portfolio Jigar Sable";
@@ -72,4 +72,5 @@ function(){
         document.title = "Come Back To Portfolio";
         $("#favicon").attr("href","/assets/images/favhand.png");
     }
-});
+ });
+ */
